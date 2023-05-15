31924f810a931d641751126f638250c58430a4a7
MykelVal <mykel.valencia@ciit.edu.ph>
2023-05-15 14:10:57 +0800

    added resources with modifiable values
    - ResourceSlider script for Stress and Productivity. starts at 0
    - ResourceTime for time resource
    - both has Context Menu functions for testing

---
+++ Assets/ResourceScripts/ResourceSlider.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResourceSlider : MonoBehaviour
+{
+    public int resourceValue;
+    public Slider resourceSlider;
+    public TextMeshProUGUI resourceText;
+
+    public void SetResource()
+    {
+        resourceSlider.value = resourceValue;                   //sets the slider's value to the current resource value - possibly remove later and just directly use resourceSlider.value
+        resourceText.text = $"{resourceSlider.value}%";         //sets the text of the resource to the current value
+    }
+
+    public void ChangeResourceValue(int toAdd)
+    {
+        resourceValue += toAdd;                                 //adds the passed value to the current resource value
+        Mathf.Clamp(resourceValue, 0, 100);                     //limits value to not exceed 0 and 100
+        SetResource();                                          //updates the current text to the new value
+    }
+
+    #region Testing
+    //you can access this by rightclicking the component and selecting the function
+    //testing change of value
+    [ContextMenu ("Add Stress")]
+    void AddStress()
+    {
+        ChangeResourceValue(12);
+    }
+    [ContextMenu("Subtract Stress")]
+    void SubtractStress()
+    {
+        ChangeResourceValue(-12);
+    }
+    #endregion
+}

4395b94ad0b932d0510da88a5b3a9e85aa0b377b
MykelVal <mykel.valencia@ciit.edu.ph>
2023-05-24 22:57:43 +0800

    fixed minor bugs
    -adjusted the focus bar to not clip off the container
    -fixed the delay when focus bar is at the edges
    -removed degradation of progress bar

--- Assets/Script/FocusMeter/FocusMeter.cs
+++ Assets/Script/FocusMeter/FocusMeter.cs
@@ -4,64 +4,91 @@ using UnityEngine;
 
 public class FocusMeter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] Transform leftPoint;
     [SerializeField] Transform rightPoint;
 
+    [Header("Tasks")]
     [SerializeField] Transform task;
-
     float taskPosition;
     float taskDestination;
-
     float turnTimer;
     [SerializeField] float timeMultiplier = 3f;
-
     float taskSpeed;
     [SerializeField] float smoothing = 1f;
 
+    [Header("Focus")]
     [SerializeField] Transform focus;
     float focusPosition;
     [SerializeField] float focusSize = 0.1f;
-    [SerializeField] float focusPower = 0.15f;
-    float focusProgress;
     float focusPullVelocity;
     [SerializeField] float focusPullPower = 0.1f;
     [SerializeField] float focusGravityPower = 0.005f;
-    [SerializeField] float focusProgressDegradationPower = 0.1f;
 
+    [Header("Progress Bar")]
+    float focusProgress;
+    [SerializeField] float focusProgressDegradationPower = 0.1f;
+    [SerializeField] float focusPower = 0.15f;
     [SerializeField] Transform progressBar;
+
+    private void Start()
+    {
+        SetSize();
+    }
     private void FixedUpdate()
     {
-        MoveTaskIcon();
-        MoveFocusBar();
-        ProgressCheck();
+        MoveTaskIcon();             //moves the chasee
+        MoveFocusBar();             //controls the chaser
+        ProgressCheck();            //controls the progress bar
+    }
+
+    private void SetSize()
+    {
+        focus.localScale = new Vector3(focusSize, 1, 1);
+        
+        /*Vector2 ls = focus.localScale;
+        float distance = Vector2.Distance(leftPoint.position, rightPoint.position);
+        ls.x = (distance / xSize * focusSize);
+        focus.localScale = ls;*/
     }
 
     private void MoveTaskIcon()
     {
-        turnTimer -= Time.deltaTime;
+        turnTimer -= Time.deltaTime;            //countdown for when the chasee/task changes position
         if (turnTimer < 0)
         {
-            turnTimer = UnityEngine.Random.value * timeMultiplier;
+            turnTimer = UnityEngine.Random.value * timeMultiplier;              //sets a random time based on the timeMultiplier (in seconds)
 
-            taskDestination = UnityEngine.Random.value;
+            taskDestination = UnityEngine.Random.value;             //sets a random value between 0 - 1 which will be the new target destination of the chasee/task
         }
 
-        taskPosition = Mathf.SmoothDamp(taskPosition, taskDestination, ref taskSpeed, smoothing);
+        taskPosition = Mathf.SmoothDamp(taskPosition, taskDestination, ref taskSpeed, smoothing);               //smoothens the movement of the chasee/task as it approaches the target destination
         task.position = Vector2.Lerp(leftPoint.position, rightPoint.position, taskPosition);
     }
 
     private void MoveFocusBar()
     {
-        if (Input.GetMouseButton(0))
+        if (focusPosition == focusSize / 2 || focusPosition >= 1 - (focusSize / 2) - 0.001)                  //sets the velocity of the chaser/focus to 0 if its at the edge
         {
-            focusPullVelocity += focusPullPower * Time.deltaTime;
+            focusPullVelocity = 0f;
         }
-        focusPullVelocity -= focusGravityPower * Time.deltaTime;
-        focusPullVelocity = Mathf.Clamp(focusPullVelocity, -0.0f, 0.01f);
+
+        if (Input.GetMouseButton(0))                //adds focusPullPower to increase the velocity
+        {
+            if (focusPosition <= 1 - focusSize / 2)
+            {
+                focusPullVelocity += focusPullPower * Time.deltaTime;
+            }
+        }
+        if (focus.position != leftPoint.position)               //subtracts focusGravityPower to decrease the velocity
+        {
+            focusPullVelocity -= focusGravityPower * Time.deltaTime;
+        }
+       
         Debug.Log(focusPullVelocity);
 
         focusPosition += focusPullVelocity;
-        focusPosition = Mathf.Clamp(focusPosition, 0, 1);
+        focusPosition = Mathf.Clamp(focusPosition, focusSize / 2, 1 - focusSize / 2);
         focus.position = Vector2.Lerp(leftPoint.position, rightPoint.position, focusPosition);
     }
 

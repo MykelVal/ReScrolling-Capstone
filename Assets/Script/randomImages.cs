0c3f3d4990eb59e4cc1a969ef81d106b9d66f094
unknown <jesriel.delmundo@ciit.edu.ph>
2023-06-01 10:58:06 +0800

    random images
    randomized images timing a little bit off but it is randomized

---
+++ Assets/Script/randomImages.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomImages : MonoBehaviour
+{
+    [SerializeField] GameObject[] imagePrefab;
+    [SerializeField] float secondSpawn = 5f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ImageSpawn());
+    }
+
+    IEnumerator ImageSpawn()
+    {
+        while (true)
+        {
+       
+            GameObject gameObject = Instantiate(imagePrefab[Random.Range(0, imagePrefab.Length)]);
+            yield return new WaitForSeconds(secondSpawn);
+            Destroy(gameObject, 2f);
+        }
+    }
+
+
+}
+
+  

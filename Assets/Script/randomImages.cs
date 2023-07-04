eb9f2df5a8416d1ea23ce438c665fb180f42e738
unknown <jesriel.delmundo@ciit.edu.ph>
2023-07-05 06:00:36 +0800

    added sfx
    button and nnews pop up sfx added

--- Assets/Script/randomImages.cs
+++ Assets/Script/randomImages.cs
@@ -6,7 +6,7 @@ public class randomImages : MonoBehaviour
 {
     [SerializeField] GameObject[] imagePrefab;
     [SerializeField] float secondSpawn = 5f;
-    public GameObject gameObject;
+  
    
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,7 @@ public class randomImages : MonoBehaviour
         while (true)
         {
        
-            gameObject = Instantiate(imagePrefab[Random.Range(0, imagePrefab.Length)]);
+            GameObject gameObject = Instantiate(imagePrefab[Random.Range(0, imagePrefab.Length)]);
             yield return new WaitForSeconds(secondSpawn);
             Destroy(gameObject, 0.1f);
         }

b9bafa45e05e9f654eba1d17c8cb1d7d8776d9be
MykelVal <mykel.valencia@ciit.edu.ph>
2023-07-06 01:55:36 +0800

    focus meter bug
    fixed focus minigame bug where the player was not flush with the edge

--- Assets/Script/FocusMeter/FocusMeter.cs
+++ Assets/Script/FocusMeter/FocusMeter.cs
@@ -112,8 +112,8 @@ public class FocusMeter : MonoBehaviour
         ls.x = focusProgress;
         progressBar.localScale = ls;
 
-        float min = focusPosition - focusSize / 2;
-        float max = focusPosition + focusSize / 2;
+        float min = focusPosition - focusSize * 2;
+        float max = focusPosition + focusSize * 2;
 
         if (min < taskPosition && taskPosition < max)
         {
@@ -158,6 +158,7 @@ public class FocusMeter : MonoBehaviour
         {
             timeObject.AddTime(240);
         }
+        Debug.Log("progress done");
 
         //return to game panel
         FindObjectOfType<UIManager>().FocusPanelToMainGame();
